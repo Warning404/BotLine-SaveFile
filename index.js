a5bb95317a1a3e3cd2800a2cbfc419b1006a58d7
Warning404 <mai.rok101@gmail.com>
2023-11-25 01:44:50 +0700

    modified:   index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.post("/webhook", line.middleware(config), (req, res) => {
 });
 
 const client = new line.Client(config);
-const axios = require("axios");
+
 
 async function sendToDiscord(
   messageId,
